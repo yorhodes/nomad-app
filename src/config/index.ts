3697731df36320fbb16b3250daf845144608ca29
Barbara Liau <barbara@liau.io>
2022-01-11 14:07:22 -0700

    lint: separate ternary into newlines

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ import { TokenMetadata, NetworkMetadata } from './config.types'
 import { SdkBaseChainConfigParams } from '@connext/nxtp-sdk'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
 const environment = process.env.VUE_APP_NOMAD_ENVIRONMENT
 const currentEnv = chooseConfig(environment)
 
@@ -11,8 +12,12 @@ export const tokens = currentEnv.tokens
 export const networks = currentEnv.networks
 export const connextConfig = currentEnv.connextConfig
 export const connextPools = currentEnv.connextPools
-export const s3URL = environment === 'development' ? 'https://nomadxyz-development-proofs.s3.us-west-2.amazonaws.com/' : 'https://nomadxyz-production-proofs.s3.us-west-2.amazonaws.com/'
-export const connextScanURL = environment === 'development' ? 'https://testnet.connextscan.io/' : 'https://connextscan.io/'
+export const s3URL = environment === 'development'
+  ? 'https://nomadxyz-development-proofs.s3.us-west-2.amazonaws.com/'
+  : 'https://nomadxyz-production-proofs.s3.us-west-2.amazonaws.com/'
+export const connextScanURL = environment === 'development'
+  ? 'https://testnet.connextscan.io/'
+  : 'https://connextscan.io/'
 
 function chooseConfig(environment: string | undefined): {
   tokens: { [key: string]: TokenMetadata }

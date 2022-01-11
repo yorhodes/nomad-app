50535d643f9d7ca75b94308d2b286259f792d132
ErinHales <mcbridee093@gmail.com>
2022-01-11 09:02:13 -0700

    add logic for install metamask

--- src/components/ErrorBoundary.vue
+++ src/components/ErrorBoundary.vue
@@ -4,20 +4,20 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useNotification } from 'naive-ui'
+// import { useNotification } from 'naive-ui'
 
 export default defineComponent({
   // TODO: consider emitting an error so that parent components
   // can determine for themselves how to handle this error instead
 
   // error boundary for any unhandled errors in child components
-  errorCaptured(err) {
-    const notification = useNotification()
+  // errorCaptured(err) {
+  //   const notification = useNotification()
 
-    notification.error({
-      title: 'Something went wrong',
-      description: (err as Error).message,
-    })
-  },
+  //   notification.error({
+  //     title: 'Something went wrong',
+  //     description: (err as Error).message,
+  //   })
+  // },
 })
 </script>

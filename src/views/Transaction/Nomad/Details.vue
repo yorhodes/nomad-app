1a92f7cd9cbc78752dad09b46038b6605b45bf87
ErinHales <mcbridee093@gmail.com>
2022-01-22 17:34:43 -0700

    add: log

--- src/views/Transaction/Nomad/Details.vue
+++ src/views/Transaction/Nomad/Details.vue
@@ -158,6 +158,7 @@ export default defineComponent({
         return
       }
       const confirmAt = await message.confirmAt()
+      console.log('confirmAt', confirmAt)
       if (this.confirmAt && !this.confirmAt.isZero()) {
         this.status = 2
         this.confirmAt = confirmAt

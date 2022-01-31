2d6c39ac4b88b2f704ab3e898810405f48a5415f
Imti Majeed <imtiazmaj@gmail.com>
2022-01-31 12:28:17 -0800

    fix: support dev/prod wallet connect rpc config values

--- src/utils/walletConnect.ts
+++ src/utils/walletConnect.ts
@@ -1,18 +1,11 @@
 import WalletConnectProvider from '@walletconnect/web3-provider'
+import { walletConnectRPCConfig } from '@/config/config.main'
 
 export async function getWalletConnectProvider() {
   // Create WalletConnect Provider
   const provider = new WalletConnectProvider({
-    // TODO: add logic for using dev vs prod values
-    rpc: {
-      1: 'https://eth-mainnet.alchemyapi.io/v2/rud551ngiIel2fRYbWmsnhwAE1FGgCLG',
-      42: 'https://eth-kovan.alchemyapi.io/v2/QKnfLTfe7CkGA80yAVsCdh8ZatQCsfHI',
-      1287: 'https://moonbeam-alpha.api.onfinality.io/rpc?apikey=44e80fe3-d9ce-40f2-8336-6089e751b625',
-      // ...
-    },
+    rpc: walletConnectRPCConfig
   })
-
   await provider.ready
-
   return provider
 }

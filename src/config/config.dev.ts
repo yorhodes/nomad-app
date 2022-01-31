a0ed4cd60ef6e174d61b2557031a4d972a8484b1
Imti Majeed <imtiazmaj@gmail.com>
2022-01-31 12:28:17 -0800

    fix: support dev/prod wallet connect rpc config values

--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -238,3 +238,8 @@ export const connextConfig: SdkBaseChainConfigParams = {
     ],
   },
 }
+
+export const walletConnectRPCConfig: Record<number, string>  = {
+  42: 'https://eth-kovan.alchemyapi.io/v2/QKnfLTfe7CkGA80yAVsCdh8ZatQCsfHI',
+  1287: 'https://moonbeam-alpha.api.onfinality.io/rpc?apikey=44e80fe3-d9ce-40f2-8336-6089e751b625',
+}
